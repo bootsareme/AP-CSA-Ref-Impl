4d253e7b03df980219f898d35f470da20e3cd27b
bootsareme <hqminecraftgaming@gmail.com>
2022-07-20 12:27:30 -0400

    Added Math.java

--- src/ap/compscia/Double.java
+++ src/ap/compscia/Double.java
@@ -1,16 +1,16 @@
 package ap.compscia;
 
 /**
- * Implementation of the Double class as specified by the AP Computer Science A Java Quick Reference standard
+ * Implementation of the Double class as specified by the AP Computer Science A Java Quick Reference standard.
  * @author  bootsareme
  * @see     java.lang.Double
  */
-public class Double {
+public final class Double {
 
     private final double value; // private field that holds the double value
 
     /**
-     * Constructs a new Double object that represents the specified double value
+     * Constructs a new Double object that represents the specified double value.
      * @param value the primitive double value
      */
     public Double(double value) {
--- src/ap/compscia/Integer.java
+++ src/ap/compscia/Integer.java
@@ -1,11 +1,11 @@
 package ap.compscia;
 
 /**
- * Implementation of the Integer class as specified by the AP Computer Science A Java Quick Reference standard
+ * Implementation of the Integer class as specified by the AP Computer Science A Java Quick Reference standard.
  * @author  bootsareme
  * @see     java.lang.Integer
  */
-public class Integer {
+public final class Integer {
 
     public static final int MAX_VALUE = 2147483647; // signed 32-bit integers cannot exceed 2^31 - 1
     public static final int MIN_VALUE = -2147483648; // signed 32-bit integers cannot go below -2^31
@@ -13,7 +13,7 @@ public class Integer {
     private final int value; // the private field that contains the integer value
 
     /**
-     * Constructs a new Integer object that represents the specified int value
+     * Constructs a new Integer object that represents the specified int value.
      * @param value the primitive integer value
      */
     public Integer(int value) {
